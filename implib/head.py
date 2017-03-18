90d0ace27c8408331a1f27f78974d3f193374329
Hilaly <hilalyamine@gmail.com>
2017-03-18 22:09:42 +0100

    Add files via upload

---
+++ implib/head.py
@@ -0,0 +1,27 @@
+from point.py import point
+from path.py import path
+from code.py import code
+from motor.py import motor, sensor, commander, cursor
+from tools.py import tools_point, tools_2d
+
+
+class draw():
+    def __init__(self, data_cursor):
+        self.Cursor = cursor(data_cursor)
+        self.CodeToExecute = code()
+        self.GCodeToExecute = []
+        self.Tool = tools_point()
+
+    def _decode_code(self):
+        self.CodeToExecute._decode_data(code)
+
+    def _decode_gcode(self):
+        pass
+
+    def _generate_road(self):
+        self.CodeToExecute._ben_ben()
+
+    def _execute(self):
+        for point in CodeToExecute.PointByPointRoad:
+            ####
+            self.Cursor._move_axe_one_step(Tool._get_axe_sens(self.Cursor.Position, point))
