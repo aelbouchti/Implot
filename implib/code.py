177b55ba45453242c39e1370b88be8dea8ba6776
Hilaly <hilalyamine@gmail.com>
2017-03-18 21:52:49 +0100

    Update code.py

--- implib/code.py
+++ implib/code.py
@@ -6,23 +6,23 @@ class code():
         self.Code = ''
         self.CheckPoints = []
         self.OperativePaths = []
-        self.PointByPointRoad=[]
-        self.Tool =  tools_point()
+        self.PointByPointRoad = []
+        self.Tool = tools_point()
     def _code(self):
         code = ''
         for i in range(0, len(self.CheckPoints)):
             a, b, c = self.CheckPoints[i]._xyz()
             code += str(self.OperativePaths[i])
             code += '.' + str(a) + '.' +str(b) + '.' + str(c)
-        self.Code=code
+        self.Code = code
 
     def _decodedata(self, code):
         pp = []
         op = []
-        self.Code=code
+        self.Code = code
         recode = code.split('.')
         for i in range(0, len(recode), 4):
-            pp += [ Point(int(recode[i]), int(recode[i+1]), int(recode[i+2]))]
+            pp += [point(int(recode[i]), int(recode[i+1]), int(recode[i+2]))]
             op += [recode[i+3]]
         self.CheckPoints = pp
         self.OperativePaths = op
@@ -33,7 +33,7 @@ class code():
         self._code()
 
     def _ben_ben(self):
-        l=len(self.CheckPoints)
+        l = len(self.CheckPoints)
         for i in range(l-1):
             ##########
-            self.PointByPointRoad+= Tool._optimise(Tool._brenseham(self.CheckPoints[i],self.CheckPoints[i+1]))
+            self.PointByPointRoad += Tool._optimise(Tool._brenseham(self.CheckPoints[i], self.CheckPoints[i+1]))
