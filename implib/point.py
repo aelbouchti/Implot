cf416d0641f88f78e4fc58bf15b09ee096eb00cf
Hilaly <hilalyamine@gmail.com>
2017-03-20 06:32:05 +0100

    Update point.py

--- implib/point.py
+++ implib/point.py
@@ -1,5 +1,6 @@
 class point():
     #declaration point xyz 3D
+    
     def __init__(self, *args):
         self.X = 0
         self.Y = 0
@@ -15,15 +16,21 @@ class point():
         self.X = self.L[0]
         self.Y = self.L[1]
         self.Z = self.L[2]
+        
     def _avx(self, value):
         self.L[0] += value
+        
     def _avy(self, value):
         self.L[1] += value
+        
     def _avz(self, value):
         self.L[2] += value
+        
     def _xyz(self):
         return (self.L[0], self.L[1], self.L[2])
+    
     def _dim(self):
         return self.dim
+    
     def _avi(self, i, value):
         self.L[i] += value
